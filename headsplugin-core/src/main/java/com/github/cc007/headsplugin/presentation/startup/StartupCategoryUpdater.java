00bd48b3dc223a9c6e71a62fa5bc8d3cace98197
CC007 <Coolcat_the_best2@Hotmail.com>
2020-07-21 20:23:16 +0200

    HPAP-6: Stagger the category updates on startup

--- headsplugin-core/src/main/java/com/github/cc007/headsplugin/presentation/startup/StartupCategoryUpdater.java
+++ headsplugin-core/src/main/java/com/github/cc007/headsplugin/presentation/startup/StartupCategoryUpdater.java
@@ -5,8 +5,11 @@ import com.github.cc007.headsplugin.api.business.services.heads.CategoryUpdater;
 import dev.alangomes.springspigot.util.scheduler.SchedulerService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+
+import java.util.Collection;
 
 @Component
 @Log4j2
@@ -16,12 +19,24 @@ public class StartupCategoryUpdater implements CommandLineRunner {
     private final CategoryUpdater categoryUpdater;
     private final SchedulerService schedulerService;
 
+    /**
+     * The delay in seconds between category updates
+     */
+    @Value("${headsplugin.categories.update.delay:10}")
+    private int delayBetweenCategoryUpdates = 10;
+
     @Override
     public void run(String... args) {
-        schedulerService.scheduleSyncDelayedTask(() -> {
-            log.info("Updating head categories...");
-            categoryUpdater.updateCategoriesIfNecessary();
-            log.info("Done updating head categories.");
-        }, 20);
+        Collection<String> updatableCategoryNames = categoryUpdater.getUpdatableCategoryNames(true);
+        log.info("Updating head categories (" + String.join(", ", updatableCategoryNames) + ")...");
+        int delay = 0;
+        for (String updatableCategoryName : updatableCategoryNames) {
+            schedulerService.scheduleSyncDelayedTask(() -> {
+                categoryUpdater.updateCategory(updatableCategoryName);
+                log.info(updatableCategoryName + " category is now updated.");
+            }, delay);
+            delay += 20 * delayBetweenCategoryUpdates;
+        }
+        log.info("All categories updates are now scheduled (with " + delayBetweenCategoryUpdates + " seconds between each category).");
     }
 }
