77fcf0dc8046203501b83c574a6f238a226dea43
CC007 <Coolcat_the_best2@Hotmail.com>
2021-12-19 19:53:05 +0100

    HPA-17: Add PredefinedCategorizable + tests

---
+++ headsplugin-rebuild/src/test/java/com/github/cc007/headsplugin/integration/daos/interfaces/PredefinedCategorizableTest.java
@@ -0,0 +1,55 @@
+package com.github.cc007.headsplugin.integration.daos.interfaces;
+
+import com.github.cc007.headsplugin.api.business.domain.Head;
+
+import lombok.NonNull;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Spy;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class PredefinedCategorizableTest {
+
+    @Spy
+    PredefinedCategorizable predefinedCategorizable = new DummyPredefinedCatagorizable();
+
+    @Test
+    void getSource() {
+        // prepare
+        String expected = "DatabaseName";
+
+        when(predefinedCategorizable.getDatabaseName())
+                .thenReturn(expected);
+
+        // execute
+        String actual = predefinedCategorizable.getSource();
+
+        // verify
+        assertThat(actual, is(expected));
+    }
+}
+
+class DummyPredefinedCatagorizable implements PredefinedCategorizable {
+
+    @Override
+    public List<Head> getCategoryHeads(@NonNull String categoryName) {
+        return null;
+    }
+
+    @Override
+    public List<String> getCategoryNames() {
+        return null;
+    }
+
+    @Override
+    public String getDatabaseName() {
+        return null;
+    }
+}
