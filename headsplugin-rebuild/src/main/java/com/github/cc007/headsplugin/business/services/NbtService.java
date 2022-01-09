62bb561e51cb5c175a01acbffade51416b6407ba
CC007 <Coolcat_the_best2@Hotmail.com>
2022-01-09 20:24:44 +0100

    HPA-17: add NbtTileEntity and NbtContainer factories to NbtService + tests
    Also changed test for `getNbtItem()`

--- headsplugin-rebuild/src/main/java/com/github/cc007/headsplugin/business/services/NbtService.java
+++ headsplugin-rebuild/src/main/java/com/github/cc007/headsplugin/business/services/NbtService.java
@@ -1,11 +1,22 @@
 package com.github.cc007.headsplugin.business.services;
 
+import de.tr7zw.changeme.nbtapi.NBTContainer;
 import de.tr7zw.changeme.nbtapi.NBTItem;
+import de.tr7zw.changeme.nbtapi.NBTTileEntity;
+import org.bukkit.block.Block;
 import org.bukkit.inventory.ItemStack;
 
 public class NbtService {
 
     public NBTItem getNbtItem(ItemStack playerHeadItemStack) {
         return new NBTItem(playerHeadItemStack);
+    }
+
+    public NBTTileEntity getNbtTileEntity(Block playerHeadBlock) {
+        return new NBTTileEntity(playerHeadBlock.getState());
+    }
+
+    public NBTContainer getNbtContainer(String nbtString) {
+        return new NBTContainer(nbtString);
     }
 }
--- headsplugin-rebuild/src/test/java/com/github/cc007/headsplugin/business/services/NbtServiceTest.java
+++ headsplugin-rebuild/src/test/java/com/github/cc007/headsplugin/business/services/NbtServiceTest.java
@@ -1,29 +1,83 @@
 package com.github.cc007.headsplugin.business.services;
 
-import de.tr7zw.changeme.nbtapi.NBTItem;
+import de.tr7zw.changeme.nbtapi.NBTContainer;
+import de.tr7zw.changeme.nbtapi.NBTTileEntity;
+import lombok.val;
+import org.bukkit.block.Block;
+import org.bukkit.block.BlockState;
 import org.bukkit.inventory.ItemStack;
 import org.junit.jupiter.api.Test;
+import org.mockito.MockedConstruction;
 
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.sameInstance;
-import static org.mockito.Mockito.doReturn;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.isA;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.mockConstruction;
+import static org.mockito.Mockito.when;
 
 class NbtServiceTest {
+
+    NbtService nbtService = new NbtService();
 
     @Test
     void getNbtItem() {
         // prepare
-        NbtService nbtService = new NbtService();
-        ItemStack itemStack = mock(ItemStack.class);
-        ItemStack itemStackClone = mock(ItemStack.class);
-        doReturn(itemStackClone)
-                .when(itemStack).clone();
+        val expected = mock(ItemStack.class);
 
-        // execute
-        NBTItem actual = nbtService.getNbtItem(itemStack);
+        try (MockedConstruction<NBTContainer> nbtTileEntity = mockConstruction(NBTContainer.class,
+                (mock, context) -> {
+                    // verify
+                    Object arg = context.arguments().get(0);
+                    assertThat(arg, isA(ItemStack.class));
+                    ItemStack actual = (ItemStack) arg;
+                    assertThat(actual, is(expected));
+                })) {
 
-        // verify
-        assertThat(actual.getItem(), sameInstance(itemStackClone));
+            // execute
+            nbtService.getNbtItem(expected);
+        }
+    }
+
+
+    @Test
+    void getNbtTileEntity() {
+        // prepare
+        val testBlock = mock(Block.class);
+        val expected = mock(BlockState.class);
+
+        when(testBlock.getState())
+                .thenReturn(expected);
+
+        try (MockedConstruction<NBTTileEntity> nbtTileEntity = mockConstruction(NBTTileEntity.class,
+                (mock, context) -> {
+                    // verify
+                    Object arg = context.arguments().get(0);
+                    assertThat(arg, isA(BlockState.class));
+                    BlockState actual = (BlockState) arg;
+                    assertThat(actual, is(expected));
+        })) {
+            // execute
+            nbtService.getNbtTileEntity(testBlock);
+        }
+    }
+
+    @Test
+    void getNbtContainer() {
+        // prepare
+        val expected = "NbtString";
+
+        try (MockedConstruction<NBTContainer> nbtTileEntity = mockConstruction(NBTContainer.class,
+                (mock, context) -> {
+                    // verify
+                    Object arg = context.arguments().get(0);
+                    assertThat(arg, isA(String.class));
+                    String actual = (String) arg;
+                    assertThat(actual, is(expected));
+                })) {
+
+            // execute
+            nbtService.getNbtContainer(expected);
+        }
     }
 }
