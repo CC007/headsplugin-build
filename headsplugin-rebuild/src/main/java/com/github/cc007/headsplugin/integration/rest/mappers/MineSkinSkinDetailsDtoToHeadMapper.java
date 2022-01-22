a162aef6e592da240baa26537f9ab98abee961b0
CC007 <Coolcat_the_best2@Hotmail.com>
2022-01-23 00:36:49 +0100

    HPA-17: add REST mappers

---
+++ headsplugin-rebuild/src/main/java/com/github/cc007/headsplugin/integration/rest/mappers/MineSkinSkinDetailsDtoToHeadMapper.java
@@ -0,0 +1,42 @@
+package com.github.cc007.headsplugin.integration.rest.mappers;
+
+import com.github.cc007.headsplugin.api.business.domain.Head;
+import com.github.cc007.headsplugin.integration.rest.dto.mineskin.SkinDetailsDto;
+
+import lombok.NonNull;
+import org.apache.commons.collections4.Transformer;
+import org.apache.commons.lang3.Validate;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+import java.util.UUID;
+
+public class MineSkinSkinDetailsDtoToHeadMapper implements Transformer<SkinDetailsDto, Head> {
+    @Override
+    public Head transform(@NonNull SkinDetailsDto skinDetailsDto) {
+        Validate.notBlank(skinDetailsDto.getName());
+        Validate.notNull(skinDetailsDto.getData());
+        Validate.notBlank(skinDetailsDto.getData().getUuid());
+        Validate.notNull(skinDetailsDto.getData().getTexture());
+        Validate.notBlank(skinDetailsDto.getData().getTexture().getValue());
+
+        return Head.builder()
+                .name(skinDetailsDto.getName())
+                .headDatabase("MineSkin")
+                .headOwner(UUID.fromString(skinDetailsDto.getData().getUuid()))
+                .value(fixValue(skinDetailsDto.getData().getTexture().getValue()))
+                .build();
+    }
+
+    /**
+     * Fix the value property by removing the containing properties before the texture property
+     *
+     * @param rawValue the base64 encoded value that should be modified
+     * @return the fixed value
+     */
+    private String fixValue(String rawValue) {
+        String decodedValue = new String(Base64.getDecoder().decode(rawValue), StandardCharsets.UTF_8);
+        String strippedDecodedValue = "{\"textures" + decodedValue.split("textures", 2)[1];
+        return Base64.getEncoder().encodeToString(strippedDecodedValue.getBytes(StandardCharsets.UTF_8));
+    }
+}
