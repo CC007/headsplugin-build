3047612f8c10dc6e5445319543fdc3226201da64
CC007 <Coolcat_the_best2@Hotmail.com>
2022-01-23 00:47:34 +0100

    HPA-17: add DAOs for MineSkin, MinecraftHeads, FreshCoal and for custom categories

---
+++ headsplugin-rebuild/src/main/java/com/github/cc007/headsplugin/integration/daos/MineSkinDao.java
@@ -0,0 +1,74 @@
+package com.github.cc007.headsplugin.integration.daos;
+
+import com.github.cc007.headsplugin.api.business.domain.Head;
+import com.github.cc007.headsplugin.config.properties.HeadspluginProperties;
+import com.github.cc007.headsplugin.integration.daos.interfaces.Creatable;
+import com.github.cc007.headsplugin.integration.daos.interfaces.Searchable;
+import com.github.cc007.headsplugin.integration.rest.dto.mineskin.SkinDetailsDto;
+import com.github.cc007.headsplugin.integration.rest.dto.mineskin.create.CreateErrorDto;
+import com.github.cc007.headsplugin.integration.rest.dto.mineskin.create.CreateSkinDetailsDto;
+import com.github.cc007.headsplugin.integration.rest.dto.mineskin.search.SkinDto;
+import com.github.cc007.headsplugin.integration.rest.feign.clients.MineSkinClient;
+
+import feign.FeignException;
+import lombok.EqualsAndHashCode;
+import lombok.RequiredArgsConstructor;
+import lombok.ToString;
+import lombok.extern.log4j.Log4j2;
+import lombok.val;
+import org.apache.commons.collections4.Transformer;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+@RequiredArgsConstructor
+@ToString
+@EqualsAndHashCode
+@Log4j2
+// TODO @ConditionalOnProperty(name = "headsplugin.provider.mineskin", havingValue = "true", matchIfMissing = true)
+public class MineSkinDao implements Searchable, Creatable {
+
+    private final MineSkinClient client;
+    private final Transformer<SkinDetailsDto, Head> headMapper;
+    private final HeadspluginProperties headspluginProperties;
+
+    @Override
+    public String getDatabaseName() {
+        return "MineSkin";
+    }
+
+    @Override
+    public Optional<Head> addHead(UUID playerUuid, String newHeadName) {
+        val createDto = client.create(playerUuid.toString(), newHeadName);
+        if (createDto instanceof CreateSkinDetailsDto createSkinDetailsDto) {
+            return Optional.of(headMapper.transform(createSkinDetailsDto));
+        }
+        if (createDto instanceof CreateErrorDto createErrorDto) {
+            log.error("Unable to add the head to MineSkin. The server responded with te following error: " + createErrorDto.getError());
+            return Optional.empty();
+        }
+        log.error("An unknown type was found when trying to parse the result from adding a head to MineSkin.");
+        return Optional.empty();
+    }
+
+    @Override
+    public List<Head> getHeads(String searchTerm) {
+        try {
+            return client.find(searchTerm)
+                    .getSkins()
+                    .stream()
+                    .map(SkinDto::getId)
+                    .map(client::findById)
+                    .map(headMapper::transform)
+                    .collect(Collectors.toList());
+        } catch (FeignException.FeignServerException ex) {
+            if (!headspluginProperties.isSuppressHttpClientErrors()) {
+                log.error(ex.getMessage(), ex);
+            }
+            return Collections.emptyList();
+        }
+    }
+}
---
+++ headsplugin-rebuild/src/main/java/com/github/cc007/headsplugin/integration/daos/MinecraftHeadsDao.java
@@ -0,0 +1,68 @@
+package com.github.cc007.headsplugin.integration.daos;
+
+import com.github.cc007.headsplugin.api.business.domain.Head;
+import com.github.cc007.headsplugin.config.properties.HeadspluginProperties;
+import com.github.cc007.headsplugin.integration.daos.interfaces.PredefinedCategorizable;
+import com.github.cc007.headsplugin.integration.rest.dto.minecraftheads.SkinDto;
+import com.github.cc007.headsplugin.integration.rest.feign.clients.MinecraftHeadsClient;
+
+import feign.FeignException;
+import lombok.EqualsAndHashCode;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.ToString;
+import lombok.extern.log4j.Log4j2;
+import org.apache.commons.collections4.Transformer;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RequiredArgsConstructor
+@ToString
+@EqualsAndHashCode
+@Log4j2
+// TODO @ConditionalOnProperty(name = "headsplugin.provider.minecraftHeads", havingValue = "true", matchIfMissing = true)
+public class MinecraftHeadsDao implements PredefinedCategorizable {
+
+    private final MinecraftHeadsClient client;
+    private final Transformer<SkinDto, Head> headMapper;
+    private final HeadspluginProperties headspluginProperties;
+
+    @Override
+    public String getDatabaseName() {
+        return "MinecraftHeads";
+    }
+
+    @Override
+    public List<Head> getCategoryHeads(@NonNull String categoryName) {
+        try {
+            return client.getCategory(categoryName)
+                    .stream()
+                    .map(headMapper::transform)
+                    .collect(Collectors.toList());
+        } catch (FeignException.FeignServerException ex) {
+            if (!headspluginProperties.isSuppressHttpClientErrors()) {
+                log.error(ex.getMessage(), ex);
+            }
+            return Collections.emptyList();
+        }
+    }
+
+    @Override
+    public List<String> getCategoryNames() {
+        return Arrays.asList(
+                "alphabet",
+                "animals",
+                "blocks",
+                "decoration",
+                "food-drinks",
+                "humans",
+                "humanoid",
+                "miscellaneous",
+                "monsters",
+                "plants"
+        );
+    }
+}
