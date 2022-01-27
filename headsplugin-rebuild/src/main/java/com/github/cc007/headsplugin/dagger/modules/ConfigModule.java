6945a0565fe97775b0f7ab1c5fbcfc2390fef5e9
CC007 <Coolcat_the_best2@Hotmail.com>
2022-01-27 15:45:10 +0100

    HPA-17: provide the plugin as a Plugin type, rather than HeadsPlugin and allow non-mapped yaml properties

--- headsplugin-rebuild/src/main/java/com/github/cc007/headsplugin/dagger/modules/ConfigModule.java
+++ headsplugin-rebuild/src/main/java/com/github/cc007/headsplugin/dagger/modules/ConfigModule.java
@@ -7,8 +7,10 @@ import com.github.cc007.headsplugin.config.properties.HeadspluginProperties;
 
 import dagger.Module;
 import dagger.Provides;
+import org.bukkit.plugin.Plugin;
 import org.yaml.snakeyaml.Yaml;
 import org.yaml.snakeyaml.constructor.CustomClassLoaderConstructor;
+import org.yaml.snakeyaml.representer.Representer;
 
 import javax.inject.Singleton;
 import java.util.Optional;
@@ -18,7 +20,7 @@ public abstract class ConfigModule {
 
     @Provides
     @Singleton
-    static HeadsPlugin provideHeadsPlugin() {
+    static Plugin providePlugin() {
         Optional<HeadsPlugin> headsPluginOptional = HeadsPlugin.getPlugin();
         if (headsPluginOptional.isEmpty()) {
             throw new IllegalStateException("HeadsPluginAPI has not been enabled yet");
@@ -28,11 +30,13 @@ public abstract class ConfigModule {
 
     @Provides
     @Singleton
-    static ConfigProperties provideConfigProperties(HeadsPlugin headsPlugin) {
+    static ConfigProperties provideConfigProperties(Plugin plugin) {
         CustomClassLoaderConstructor configPropertiesClassLoader = new CustomClassLoaderConstructor(ConfigProperties.class.getClassLoader());
         configPropertiesClassLoader.getPropertyUtils().setSkipMissingProperties(true);
-        Yaml yaml = new Yaml(configPropertiesClassLoader);
-        return yaml.loadAs(headsPlugin.getResource("config.yml"), ConfigProperties.class);
+        Representer representer = new Representer();
+        representer.getPropertyUtils().setSkipMissingProperties(true);
+        Yaml yaml = new Yaml(configPropertiesClassLoader, representer);
+        return yaml.loadAs(plugin.getResource("config.yml"), ConfigProperties.class);
     }
 
     @Provides
