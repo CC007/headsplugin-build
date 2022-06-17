b94510bba0306dc061c313a7f6849882b232c5b5
CC007 <Coolcat_the_best2@Hotmail.com>
2022-06-17 22:00:10 +0200

    HPA-0: move headsplugin-rebuild to headsplugin-core
    rebuild is done and has become the new core.

--- headsplugin-core/src/main/java/com/github/cc007/headsplugin/dagger/modules/source/CustomCategoriesModule.java
+++ headsplugin-core/src/main/java/com/github/cc007/headsplugin/dagger/modules/source/CustomCategoriesModule.java
@@ -1,17 +1,11 @@
 package com.github.cc007.headsplugin.dagger.modules.source;
 
-import com.github.cc007.headsplugin.api.business.domain.Head;
 import com.github.cc007.headsplugin.api.business.services.heads.HeadSearcher;
 import com.github.cc007.headsplugin.config.properties.CategoriesProperties;
-import com.github.cc007.headsplugin.config.properties.HeadspluginProperties;
 import com.github.cc007.headsplugin.integration.daos.CustomCategoriesDao;
-import com.github.cc007.headsplugin.integration.daos.FreshCoalDao;
-import com.github.cc007.headsplugin.integration.rest.dto.freshcoal.SkinDto;
-import com.github.cc007.headsplugin.integration.rest.feign.clients.FreshCoalClient;
-import com.github.cc007.headsplugin.integration.rest.mappers.FreshCoalSkinDtoToHeadMapper;
+
 import dagger.Module;
 import dagger.Provides;
-import org.apache.commons.collections4.Transformer;
 
 import javax.inject.Singleton;
 
